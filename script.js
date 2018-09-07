5eb851c04839dcb28bb8abe3a337185f6ed94633
Celine Kim <kimc457@newschool.edu>
2018-09-06 22:13:36 -0400

    initial commit
    i hope this works

---
+++ script.js
@@ -0,0 +1,99 @@
+script.js
+
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+console.log('hi');
+
+
+
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
+
+
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $("p").hide(1000);
+//     });
+// });
+
+
+$(".box1").hide();
+
+
+$(document).ready(function(){
+    $(".button1").click(function(){
+        $(".box1").toggle(1000);
+    });
+});
+
+$(".box2").hide();
+
+$(document).ready(function(){
+    $(".button2").click(function(){
+        $(".box2").toggle(1000);
+    });
+});
+
+$(".box3").hide();
+$(document).ready(function(){
+    $(".button3").click(function(){
+        $(".box3").toggle(1000);
+    });
+});
+
+
+$(".box4").hide();
+$(document).ready(function(){
+    $(".button4").click(function(){
+        $(".box4").toggle(1000);
+    });
+});
+
+
+$(".box5").hide();
+$(document).ready(function(){
+    $(".button5").click(function(){
+        $(".box5").toggle(1000);
+    });
+});
+
+
+$(".box6").hide();
+$(document).ready(function(){
+    $(".button6").click(function(){
+        $(".box6").toggle(1000);
+    });
+});
+
+
+$(".box7").hide();
+$(document).ready(function(){
+    $(".button7").click(function(){
+        $(".box7").toggle(1000);
+    });
+});
+
+
+$(".box8").hide();
+$(document).ready(function(){
+    $(".button8").click(function(){
+        $(".box8").toggle(1000);
+    });
+});
+
+
+$(".box9").hide();
+$(document).ready(function(){
+    $(".button9").click(function(){
+        $(".box9").toggle(1000);
+    });
+});
